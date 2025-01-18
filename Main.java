87805cf148c6883cdb19797c5470a8f9b3e90fe1
Hari-Sri-T <harisrishanmukhasai2005@gmail.com>
2025-01-19 03:34:35 +0530

    Added Switch Case in Main
    added a switch case to select one of the 4 methods (Addition,
    Substraction, Multiplication, Division)

--- Main.java
+++ Main.java
@@ -6,13 +6,36 @@ import java.util.Scanner;
 
 class Main{
 	public static void main(String args[]){
-
-		Calculator cal = new Calculator();
-	    System.out.println("Sum is: "+cal.addition());
-		System.out.println("Difference is: "+cal.substraction());
-		System.out.println("Multiplication is: "+cal.multiplication());
-		System.out.println("Division is: "+cal.division());
+		Scanner scan = new Scanner(System.in);
 		
-	}
+		System.out.println("Select what to do \na)Addition \nb)Substraction \nc)Multiplication \nd)Division");
+		String method = scan.nextLine();
+		System.out.println("You Selected: "+method);
+		
+		Calculator cal = new Calculator();
+		
+		switch(method){
+			
+			case "a":
+				System.out.println("Sum is: "+cal.addition());
+				break;
+				
+			case "b":
+				System.out.println("Difference is: "+cal.substraction());
+				break;
+				
+			case "c":
+				System.out.println("Multiplication is: "+cal.multiplication());
+				break;
+				
+			case "d":
+				System.out.println("Division is: "+cal.division());
+				break;
+				
+			default:
+				System.out.println("Invalid Input");
+				break;
+	    }
+    }
 }
 	
